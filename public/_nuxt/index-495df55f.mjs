82d084df651008b9e25345bab8ce0f0a22fa667d
Seok-Hun <43300837+Seok-Hun@users.noreply.github.com>
2022-09-03 03:35:57 +0900

    Add files via upload

---
+++ public/_nuxt/index-495df55f.mjs
@@ -0,0 +1,1 @@
+import{_ as e}from"./entry-c9d303b2.mjs";const r={};function n(t,_){return" this is index "}var s=e(r,[["render",n]]);export{s as default};
---
+++ server/chunks/client.manifest.mjs
@@ -0,0 +1,24 @@
+const client_manifest = {
+  "node_modules/nuxt/dist/app/entry.mjs": {
+    "file": "entry-c9d303b2.mjs",
+    "src": "node_modules/nuxt/dist/app/entry.mjs",
+    "isEntry": true,
+    "dynamicImports": [
+      "pages/index.vue"
+    ],
+    "css": [
+      "entry.e7372e90.css"
+    ]
+  },
+  "pages/index.vue": {
+    "file": "index-495df55f.mjs",
+    "src": "pages/index.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.mjs"
+    ]
+  }
+};
+
+export { client_manifest as default };
+//# sourceMappingURL=client.manifest.mjs.map
